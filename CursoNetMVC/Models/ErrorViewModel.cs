334ae251f06c444554e820e575def04f1c631165
Yael Chavez <you400@gmail.com>
2025-08-09 10:10:22 -0600

    Agregar archivos de proyecto.

---
+++ CursoNetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CursoNetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
